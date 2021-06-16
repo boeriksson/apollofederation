8cdf7cbaa2dfe0d6daa0eeae649a2be5737de55c
boeriksson <bo.sandevind@tele2.com>
2021-06-16 17:12:38 +0200

    apollo client..

--- services/main-site/src/fe/Main.js
+++ services/main-site/src/fe/Main.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import styled from '@emotion/styled'
 import {
     useQuery,
     gql
 } from '@apollo/client'
+
+const CustomerEntry = styled.div`
+  margin: 5px;
+  padding: 5px;
+  border-bottom: 1px solid #222;
+  font: 1.1em "Fira Sans", sans-serif;
+`
+const CustomerTitle = styled.div`
+  cursor: pointer;
+  display: flex;
+  font-weight: 500;
+
+  div:nth-of-type(1) {
+    margin-right: 10px;
+  }
+`
+
+const Expanded = styled.div`
+  height: 100px;
+`
 
 const CUSTOMERS = gql`
   query GetCustomers {
@@ -15,23 +36,29 @@ const CUSTOMERS = gql`
 
 const Main = () => {
     const {loading, error, data} = useQuery(CUSTOMERS)
-    if (loading) return <p>Loading...</p>
-    if (error) return <p>Error :(</p>
+    if (error) {
+        return <div>Error: {error}</div>
+    }
+    if (loading) {
+        return <div>Loading...</div>
+    }
 
-    const customers = data.customers.map(({name, id}) => (
-        <div key={id}>
-            <p>
-                {name}
-            </p>
-        </div>
+    const handleClick = (id) => {
+        console.log(id)
+    }
+
+    const customers = data.customers.map(({id, name}) => (
+        <CustomerEntry key={id} onClick={() => handleClick(id)}>
+            <CustomerTitle>
+                <div>{id}</div>
+                <div>{name}</div>
+            </CustomerTitle>
+        </CustomerEntry>
     ))
 
-    return (
-        <>
-            <div>Hello World!</div>
-            {customers}
-        </>
-    )
+    return <>
+        {customers}
+    </>
 }
 
 export default Main
